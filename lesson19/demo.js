5f4801661e3c06687d952947fd6007735c59e969
murtazaali99 <murtazazaidi888>
2025-07-08 12:23:54 +0500

    hellllo

---
+++ lesson19/demo.js
@@ -0,0 +1,14 @@
+// the below code is doing a suryey
+var a = zaidi
+var a = 103 
+
+console.log(b)
+
+var c = 97
+
+var d = b+c
+console.log(d)
+console.log(b / c)
+console.log(b - c)
+console.log(b * c)
+console.log(b % c)
