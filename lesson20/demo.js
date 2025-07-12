094e550877ad461ad961aacdf9580197d24c4863
murtazaali99 <murtazazaidi888>
2025-07-12 10:44:10 +0500

    j

---
+++ lesson20/demo.js
@@ -0,0 +1,25 @@
+function gotoplay(){
+    console.log("hi sir good morning can i go to play cricket")
+    console.log("please sir,please me go i will comebackon time")
+}
+gotoplay()
+gotoplay()
+gotoplay()
+gotoplay()
+gotoplay()
+gotoplay()
+gotoplay()
+
+function sum(x,y,z){
+    var result=x+y+z
+    console.log(result)
+}
+sum(12,14,16)
+sum(1,3,5)
+
+function multiply(x,y,z,a){
+    var zaidi=x*y*z*a
+    return zaidi
+}
+ var k=multiply(2,4,6,8)
+ console.log(k)
