6bffe7124ef9f97b5f7489e5fbd8713230260132
murtazaali99 <murtazazaidi888>
2025-07-19 10:53:34 +0500

    hellllo

---
+++ lesson22/demo.js
@@ -0,0 +1,16 @@
+var a={gogo:56,zaidi:"king in cricket", asia:["Pakistan,India,Bangladesh,China"], projectdone:true,printzaidi:function(){return"full name of zaidi"},zaididemond:12+14}
+
+console.log(a.zaidi)
+console.log(a.gogo)
+console.log(a.projectdone)
+console.log(a.printzaidi())
+console.log(a.zaididemond)
+
+console.log(Math.random)
+console.log(Math.PI)
+
+var b= "bat and ball"
+console.log(b.toUppercase())
+
+var c= 2
+console.log(c.toExponential(4))
