30d6522b9888a93b8a0afc8bee75a6e6f6fd4b2f
murtazaali99 <murtazazaidi888>
2025-07-20 11:02:54 +0500

    helllllllllllo

---
+++ lesson22/project.js
@@ -0,0 +1,18 @@
+var a={gogo:68,pencil:"used to write something",asia:["China,Japan,Indonesia,Malaysia,India,Pakistan,Napal"], projectdone:false,printali:function(){return "full name of ali"},syeddemond:50+50,syed:"danger play in game"}
+
+console.log(a.syed)
+console.log(a.pencil)
+console.log(a.gogo)
+console.log(a.project)
+console.log(a.syed)
+console.log(a.projectdone)
+console.log(a.printali())
+
+console.log(Math.random)
+console.log(Math.PI)
+
+var b="bat and ball"
+console.log(b.toUpperCase())
+
+var c= 2
+console.log(c.toExponential(6))
