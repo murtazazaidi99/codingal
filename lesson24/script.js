701e06fc62536acacbc053f3697ab37cfc69a4c8
murtazaali99 <murtazazaidi888>
2025-07-26 10:55:46 +0500

    murtaza

---
+++ lesson24/script.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("zaidi")[0].style.backgroundColor = "green"
+
+document.getElementById("syed").innerHTML ="hello howare you"
+
+document.getElementsByTagName("h4")[0].innerHTML="supppppppper man"
