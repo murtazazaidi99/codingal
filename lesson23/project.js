d843a8c43c7f553df3a83733da70a422f8113dba
murtazaali99 <murtazazaidi888>
2025-07-26 10:03:21 +0500

    hiiiiiiiiiiii

---
+++ lesson23/project.js
@@ -0,0 +1,30 @@
+var a=['lion','elephant','snake','panda','tiger']
+
+ console.log(a[0])
+ console.log(a[2])
+ console.log(a[1])
+
+ var groupage=[20,40,70,90,20,40,60]
+ console.log(groupage[0])
+ console.log(groupage[4])
+ console.log(groupage[3])
+ console.log(groupage[5])
+
+ var boolarray=[true,false,false,true,false]
+
+ var objarray=[{a:3456,b:6543,leo:"it is a cat name in our home"},{sheep:"we eat a meal of sheep",meal:"meal is a protein"},{Name:"zaidi",income:"high"}]
+
+ console.log(objarray[2].Name)
+
+ console.log(a.join("_"))
+ a.pop()
+ console.log(a.join(" "))
+
+ function sum(a,b,c){
+    return a+b+c
+ }
+ function average(a,b,c){
+    return (a,b,c)/3
+ }
+ const result= average(4,6,8)
+ console.log(result)
