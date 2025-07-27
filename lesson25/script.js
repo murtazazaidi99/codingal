5621983b80b932628fa22d4fdaef8b85fcc3fb00
murtazaali99 <murtazazaidi888>
2025-07-27 11:03:06 +0500

    hi

---
+++ lesson25/script.js
@@ -0,0 +1,21 @@
+var a =90
+console.log(typeof(a))
+var b='today is a cool day'
+console.log(typeof(b))
+var c=false
+console.log(typeof(c))
+var d={Name:"zaidi",age:60}
+console.log(typeof(d))
+var e=[100,200,300,400,500,600,700,800,900,1000]
+console.log(typeof(e))
+var f=null
+var g=undefined
+
+if(a>1000){
+    console.log("hello i am going")
+}else if(a>99){
+    console.log("hi how are you doing")
+}
+else{
+    console.log("oh rain were start")
+}
