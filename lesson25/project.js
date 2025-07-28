7a8e6df461e42db8304897a34716a497bfb3a336
murtazaali99 <murtazazaidi888>
2025-07-28 08:40:03 +0500

    murtaza

--- lesson25/project.js
+++ lesson25/project.js
@@ -1,12 +1,12 @@
 var a =90
 console.log(typeof(a))
-var b="today is a cool day"
+var b="today is a hot day"
 console.log(typeof(b))
-var c=false
+var c=true
 console.log(typeof(c))
-var d={Name:"zaidi",age:60}
+var d={Name:"ali",age:40}
 console.log(typeof(d))
-var e=[100,200,300,400,500,600,700,800,900,1000]
+var e=[100,200,,400,,600,700,800,,1000]
 console.log(typeof(e))
 var f=null
 var g=undefined
@@ -24,14 +24,14 @@ for(i=0;i=1000;i++){
     console.log("BBQ")
 } 
 
-var w = "www.cricket.com"
+var w = "www.football.com"
 
-let k=67000
-k= 80
+let k=77000
+k= 60
 k="o my god"
-k=[10,30,50]
+k=[10,30,60]
 
-const z="hero"
-z='donkey'
+const z="horce"
+z='monkey'
 
 console.log(k)
