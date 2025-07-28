66d77dd4d43c4eb8f391a2e509cd25a7bc390fa9
murtazaali99 <murtazazaidi888>
2025-07-28 08:17:45 +0500

    murtaza

---
+++ lesson24/project script.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("ronaldo")[0].style.backgroundColor = "blue"
+
+document.getElementById("murtaza").innerHTML="nice to meet you"
+
+document.getElementsByTagName("h3")[0].innerHTML="how are you doing"
