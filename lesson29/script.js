82975bb490829f0791a3574b81a7174f5efb28ce
murtazaali99 <murtazazaidi888>
2025-08-10 10:58:22 +0500

    helllo

---
+++ lesson29/script.js
@@ -0,0 +1,12 @@
+//json:javascript object notation
+var a={name:'zaiid',age:23,height:6,gun:true,look:'good'}
+
+console.log(a)
+
+var b=JSON.stringify(a)
+
+console.log(b)
+
+var c=JSON.parse(b)
+
+console.log(c)
