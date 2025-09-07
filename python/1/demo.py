62725a2b6504d3075f0507b420d772e18f63761e
murtazaali99 <murtazazaidi888>
2025-09-07 11:11:34 +0500

    hello

---
+++ python/1/demo.py
@@ -0,0 +1,8 @@
+# this is a comment which is not run in code
+print('hello how are you i am a python the most popular in the word')
+
+a = 40
+b ="elephant is very big as compared to mouse"
+c = 6.500
+d = True
+e = False
