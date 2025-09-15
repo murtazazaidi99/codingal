4d1ef91a7eb711c14079a68e11b1c16e44828c17
murtazaali99 <murtazazaidi888>
2025-09-15 20:05:41 +0500

    hello

---
+++ python/1/2activity.py
@@ -0,0 +1,12 @@
+num1=40
+num2=60
+
+add=num1+num2
+sub=num1-num2
+multiply=num1*num2
+divide=num1/num2
+
+print("addition of 2 number",add)
+print("subtraction of 2 number",sub)
+print("multiplication of 2 number",multiply)
+print("division of 2 number",divide)
