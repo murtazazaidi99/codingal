f9b733cc6b2647bd81df0e05c94a1c7d5a95c3b5
murtazaali99 <murtazazaidi888>
2025-09-17 18:21:14 +0500

    helllo

--- python/1/activity1.py
+++ python/1/activity1.py
@@ -1,7 +1,7 @@
 name="zaidi" #string datatype
 age="17" #int,number
 height="5.9"#float
-isstudent=False#boolean
+isstudent=True#boolean
 print("hello my name",name)
 print("i am " ,age,"year old")
 print("my height is ",height)
@@ -13,4 +13,4 @@ print("datatype of height",type(height))
 print("datatype of isstudent ",type(isstudent))
 
 age=str(age)
-print("datatype of age",type(age))
+print("datatype of age",type(age)
