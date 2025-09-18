517144371d03b60acb9d3aa2c4e2229ad5674bc7
murtazaali99 <murtazazaidi888>
2025-09-18 20:16:44 +0500

    hello

---
+++ python/7activity.py
@@ -0,0 +1,8 @@
+num=int(input("enter a number"))
+if num>60:
+    if num%2==0:
+    print("number is greater then 60 and its even")
+    else:
+        print("number is greater then 60 but its odd ")
+else:
+    print("number is less then 60")        
