fba7ea667d7b2729818291da319cfbd070459f6f
murtazaali99 <murtazazaidi888>
2025-09-18 19:28:40 +0500

    hello

---
+++ python/demoactivite.py
@@ -0,0 +1,13 @@
+num1=100
+num2=50
+num=300
+
+add=num1+num2+num3
+sub=num1-num2-num3
+multiply=num1*num2*num3
+divide=num1/num2/num3
+
+print("addition of 2 number",add)
+print("subtraction of 2 number",sub)
+print("multiplication of 2 number",multiply)
+print("division of 2 number",divide)
