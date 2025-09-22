efc1e41612f96b2cc455773cb086ff38bda10f3a
murtazaali99 <murtazazaidi888>
2025-09-22 19:03:21 +0500

    hello

---
+++ python/demoactivity2.py
@@ -0,0 +1,8 @@
+num=int(input("enter a number"))
+if num>610:
+    if num%2==0:
+    print("number is greater then 610 and its even")
+    else:
+        print("number is greater then 610 but its odd ")
+else:
+    print("number is less then 610")        
