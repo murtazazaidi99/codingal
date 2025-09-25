9e7d8ff9482d6a8418d688a44e2f2cb9b31c1528
murtazaali99 <murtazazaidi888>
2025-09-25 20:09:24 +0500

    hello

---
+++ python/14activity.py
@@ -0,0 +1,20 @@
+#this function adds two numbers
+def add(x,y):
+   return x+y
+#this function subtract two numbers
+def subtract(x,y):
+   return x-y
+#this function multiplies two numbers
+def add(x,y):
+   return x*y
+   #this function divide two numbers
+def add(x,y):
+   return x/y
+
+num1= int(input("enter number 1:"))
+num2= int(input("enter number 2:"))
+
+print("sum:",add(num1,num2))
+print("difference:",subtract(num1,num2))
+print("product:",multiply(num1,num2))
+print("quotient:",divid(num1,num2))
