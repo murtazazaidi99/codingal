607e9e3699fd3038a1a30affa1aad27ba2b58cc3
murtazaali99 <murtazazaidi888>
2025-09-29 20:20:51 +0500

    hello

---
+++ python/17activity.py
@@ -0,0 +1,20 @@
+import turtle
+t=turtle.Turtle()
+s= turtle.Screen()
+colors=["red","blue","purple","orange","green"]
+s.bgcolor("black")
+t.speed("fastest")
+t.hideturtle()
+while True:
+    for x in range(200):
+        t.pencolor(colors[x%len(colors)])
+        t.width(x/100+1)
+        t.forward(x)
+        t.left(59)
+    t.right(239)
+    for x in range(200,0,-1):
+        t.pencolors("black")
+        t.width(x/100+7)
+        t.forward(x)
+        t.right(59)
+turtle.done()
