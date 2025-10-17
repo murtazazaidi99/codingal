4d9737f4cd830739f947e6e7d12d588a39d4673b
murtazaali99 <murtazazaidi888>
2025-10-17 19:11:25 +0500

    hello

---
+++ python2/demo3.py
@@ -0,0 +1,18 @@
+abc=[3,5,7,9,13,14,126,111,504]
+
+print(abc[5])
+
+xyz=['badman','ronaldo','messi','zaidi',]
+
+print(xyz[2])
+print(xyz[4])
+
+
+print(len(abc))
+
+xyz[0]='Gum men'
+
+print(xyz[0])
+
+for i in range(len(xyz)):
+    print(xyz[i])
