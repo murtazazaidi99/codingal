253677a13944be0eaa401c463483becc2bffa672
murtazaali99 <murtazazaidi888>
2025-11-03 20:57:33 +0500

    hello

--- python2/demo5.py
+++ python2/demo5.py
@@ -8,4 +8,4 @@ mouse_obj=Animals('black','small')
 
 
 print(f'dianasor color: {dianasor_obj.color}, size: {dianasor_obj.size}')
-print(f'mouse color: {mouse_obj.color}, size: {mouse_obj.size}')
+print(f'mouse color: {mouse_obj.color}, size: {mouse_obj.size'})
---
+++ python2/demo6.py
@@ -0,0 +1,9 @@
+class A:
+    def __init__(self,age,name):
+        self.age=age
+        self.name=name
+        print('hello my name is',self.name)
+
+    def J(self):
+        print("hello")
+obj=A ("zaidi",17)
