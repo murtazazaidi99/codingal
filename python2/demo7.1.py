ab663da1caf50ff628c2a6e85d7001dcbeb285cc
murtazaali99 <murtazazaidi888>
2025-11-06 21:26:53 +0500

    helo

--- python2/demo7.1.py
+++ python2/demo7.1.py
@@ -9,4 +9,33 @@ class Dog (Animal):
 
 dog=Dog()
 dog.speak()
-#
+# types of Inheritence
+
+#single Inheritence
+ class Animals:
+    def speak(self):
+        print("Animal make sound")
+
+class Dog (Animal):
+    def speak (self):
+        print("Dog barks")
+
+dog=Dog()
+dog.speak()
+
+#multiple Inheritence
+class Father:
+    def quality(self):
+        print("father is hard working")
+
+class Mother:
+    def nature(self):
+        print("mother is care")
+class child:
+    def talent(self):
+        print("child is talented")
+
+c=child()
+c.quality()
+c.nature()
+c.talent()
