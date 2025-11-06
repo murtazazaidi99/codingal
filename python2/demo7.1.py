54e46021b3cd7b209c99d576ad1c6803f635e5c4
murtazaali99 <murtazazaidi888>
2025-11-06 21:18:42 +0500

    helo

---
+++ python2/demo7.1.py
@@ -0,0 +1,12 @@
+# Inheritence
+ class Animals:
+    def speak(self):
+        print("Animal make sound")
+
+class Dog (Animal):
+    def speak (self):
+        print("Dog barks")
+
+dog=Dog()
+dog.speak()
+#
