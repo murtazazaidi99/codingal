ef185354de261d9d4a4e946fae17341fefe2a213
murtazaali99 <murtazazaidi888>
2025-12-08 20:56:47 +0500

    helo

---
+++ python3/activity6.py
@@ -0,0 +1,9 @@
+def fun1(n):
+    return n*(n+1)/7
+print (fun1(14))
+def fun3(n):
+    sum =0
+    for i in range(1,n+1):
+        for j in range(i,i+1):
+            sum += 5
+            return sum
