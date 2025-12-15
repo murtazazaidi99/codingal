8dcf430335c1eb9f5e2f3a9407ced34a559f4f69
murtazaali99 <murtazazaidi888>
2025-12-15 21:02:52 +0500

    hello

---
+++ python3/lesson8.py
@@ -0,0 +1,18 @@
+#t(n)=t(n/2)+0(1)
+
+def print (n):
+    if (n<=0):
+        return
+    print("codingal")
+    print(n/2)
+
+def sum(n):
+    return n *(n+1)/2
+def arraySum(a):
+    sum=0
+    for i in a:
+        sum =sum +!
+
+    return(sum)
+    a=[12,14,16]
+arraySum(a)
