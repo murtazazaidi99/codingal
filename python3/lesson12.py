34e469df799f9ee3e12f7190883345239b12a396
murtazaali99 <murtazazaidi888>
2025-12-29 21:06:29 +0500

    hellllo

---
+++ python3/lesson12.py
@@ -0,0 +1,41 @@
+num1 = 20
+num2 = 90
+print("num1 & num2 are :",num1,num2)
+print("num1 | num2 are :",num1 | num2)
+print("num1 ^ num2 are :",num1^num2)
+print("num1 << num2 :",num1<<num2)
+print("num1 >> num2 :",num1>>num2)
+print("~num1 are:",~num1)
+print("~num2 are:",num2)
+
+
+#second activity
+def isEvenOdd(n):
+    if (n^1 == n+1):
+        return True;
+    else:
+        return False;
+number = int(input("enter a number:"))
+if isEvenOdd(number):
+   print(number,"is even")
+else:
+   print(number,"is odd")
+
+
+#thrid activity
+num1 =20
+num2 =40
+number = int(input("enter a number:"))
+if isEvenOdd(number):
+   print(number,"is even")
+else:
+   print(number,"is odd")
+
+print("num1 & num2 are :",num1,num2)
+print("num1 | num2 are :",num1 | num2)
+print("num1 ^ num2 are :",num1^num2)
+print("num1 << num2 :",num1<<num2)
+print("num1 >> num2 :",num1>>num2)
+print("~num1 are:",~num1)
+print("~num2 are:",num2)
+
