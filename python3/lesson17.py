0d781ff9b79273f69b73dcc619bd79203ed694ca
murtazaali99 <murtazazaidi888>
2026-01-19 21:15:40 +0500

    hello

--- python3/lesson17.py
+++ python3/lesson17.py
@@ -6,26 +6,25 @@ def power_set(s):
     for r in range(len(s)+1):
         for combo in combinations(s,r):
             result.append(set(combo))
-    return
+    return result
 # Example
 A={1,2,3}
 ps=power_set(A)
 print(ps)
 print("Total subsets:", len(ps))
 
-# Power set of a set
-#Method 2:Bit mainpilation
-
-def power_set_bits(arr):
-    n=len(arr)
-    res=[]
-    for mask in range(1<<n):
-        for i in range(n):
-            if mask&(1<<i):
-                subset.append(arr[i])
-        res.append(subset)
-        return res
-print(power_set_bits([1,2,3]))
+# # Power set of a set
+# #Method 2:Bit mainpilation
+#  def power_set_bits(arr):
+#      n=len(arr)
+#      res=[]
+#      for mask in range(1<<n):
+#          for i in range(n):
+#              if mask&(1<<i):
+#                  subset.append(arr[i])
+#          res.append(subset)
+#          return res
+#  # print(power_set_bits([1,2,3]))
 
 # Activity
 def bits_to_change(a,b):
@@ -34,4 +33,4 @@ def bits_to_change(a,b):
 # Example
 a=29 #11101
 b=15 #01111
-print(bits-to_change(a,b)) #output:2
+print(bits_to_change(a,b)) #output:2
